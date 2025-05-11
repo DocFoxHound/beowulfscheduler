ff55951449e73a5f02e9392d1f6d152c527432c3
DocFoxHound <martin.medic@hotmail.com>
2025-05-11 22:41:21 +0300

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
